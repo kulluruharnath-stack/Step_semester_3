b14458356e2e7814e6a6d3eae92728ccc3f2ed2d
Kulluru Harnath <kulluruharnath@gmail.com>
2026-09-06 01:21:36 +0530

    Add Problem2 maximum sub array

---
+++ Problem2W4.java
@@ -0,0 +1,24 @@
+public class Problem2W4 {
+
+    public static int maxSubArray(int[] nums) {
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            // Extend the current subarray or start a new one
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+
+            // Update maximum sum
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        System.out.println(maxSubArray(nums));
+    }
+}
