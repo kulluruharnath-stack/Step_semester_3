bf4c8cabf3fe692ffa346b9888465fe923ddf843
Kulluru Harnath <kulluruharnath@gmail.com>
2026-09-19 16:53:49 +0530

    Add problem3 to get password strength

---
+++ Problem3W7.java
@@ -0,0 +1,26 @@
+class PasswordChecker {
+    private final String password;
+
+    PasswordChecker(String password) {
+        this.password = password;
+    }
+
+    String getStrength() {
+        if (password.length() < 6)
+            return "Weak";
+        else if (password.length() <= 9)
+            return "Medium";
+        else
+            return "Strong";
+    }
+}
+
+public class Problem3W7 {
+    public static void main(String[] args) {
+        PasswordChecker pc = new PasswordChecker("abcd");
+        System.out.println(pc.getStrength());
+
+        PasswordChecker pc2 = new PasswordChecker("abcdefghij");
+        System.out.println(pc2.getStrength());
+    }
+}
